bd51ae23351f9df2c7653b36a6f60cd833135dba
TonimatasDEV <amatascubero@gmail.com>
2024-02-01 21:38:39 +0100

    Basic bot loading logic

--- api/src/main/java/dev/tonimatas/discordmk/api/Bot.java
+++ api/src/main/java/dev/tonimatas/discordmk/api/Bot.java
@@ -1,12 +1,17 @@
 package dev.tonimatas.discordmk.api;
 
-import java.io.File;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
 
 // TODO: Add 1000 lines
 public class Bot {
-    protected String filePath;
+    protected String token;
+    protected JDA jda;
     
-    public Bot(File filePath) {
-        this.filePath = filePath.getPath();
+    public Bot(String token) {
+        this.token = token;
+
+        JDABuilder jdaBuilder = JDABuilder.createDefault(token);
+        this.jda = jdaBuilder.build();
     }
 }
--- server/src/main/java/dev/tonimatas/discordmk/reader/ReaderMK.java
+++ server/src/main/java/dev/tonimatas/discordmk/reader/ReaderMK.java
@@ -1,8 +1,12 @@
 package dev.tonimatas.discordmk.reader;
 
+import com.google.gson.JsonObject;
+import dev.tonimatas.discordmk.Main;
 import dev.tonimatas.discordmk.api.Bot;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 
 public class ReaderMK {
     protected File jsonFile;
@@ -12,7 +16,18 @@ public class ReaderMK {
     }
     
     public Bot build() {
+        JsonObject jsonObject;
+        try {
+            jsonObject = Main.gson.fromJson(new FileReader(this.jsonFile), JsonObject.class);
+        } catch (FileNotFoundException e) {
+            System.out.println("Error on read JSON");
+            return null;
+        }
+        
+        String token = jsonObject.get("token").getAsString();
+        
+
         // Create all read logic
-        return new Bot(jsonFile);
+        return new Bot(token);
     }
 }
