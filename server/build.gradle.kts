34e3bcce2fad15b318363cdfdf1018c148529f79
TonimatasDEV <79017964+TonimatasDEV@users.noreply.github.com>
2024-02-01 21:38:39 +0100

    Basic bot loading logic

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
 val jdaVersion: String by extra
 
 dependencies {
+    implementation("com.google.code.gson:gson:2.10.1")
     implementation("net.dv8tion:JDA:$jdaVersion")
     implementation(project(":api"))
 }
--- server/src/main/java/dev/tonimatas/discordmk/Main.java
+++ server/src/main/java/dev/tonimatas/discordmk/Main.java
@@ -1,7 +1,34 @@
 package dev.tonimatas.discordmk;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import dev.tonimatas.discordmk.api.Bot;
+import dev.tonimatas.discordmk.reader.ReaderMK;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
+    public static Gson gson = new GsonBuilder().setPrettyPrinting().create();
+    public static List<Bot> bots = new ArrayList<>();
+    
+    @SuppressWarnings("ResultOfMethodCallIgnored")
     public static void main(String[] args) {
+        File file = new File("bots");
         
+        if (!file.exists()) file.mkdir();
+        
+        File[] files = file.listFiles();
+        if (files == null) {
+            System.out.println("No bot to load.");
+        } else {
+            Arrays.stream(files).forEach(botFile -> bots.add(new ReaderMK(botFile).build()));
+        }
+        
+        // TODO: Add the server logic to connect with the Controller.
+
+        System.out.println("Server started successfully.");
     }
 }
