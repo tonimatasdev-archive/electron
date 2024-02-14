9bc0f59d43c01c3c67234aff6601d7c06b4c80f6
TonimatasDEV <79017964+TonimatasDEV@users.noreply.github.com>
2024-02-14 20:29:50 +0100

    Basic networking

---
+++ common/src/main/java/dev/tonimatas/discordmk/network/Messages.java
@@ -0,0 +1,13 @@
+package dev.tonimatas.discordmk.network;
+
+import java.net.Socket;
+
+public class Messages {
+    public static void send(Socket socket) {
+
+    }
+
+    public static void receive(Socket socket) {
+
+    }
+}
--- controller/src/main/java/dev/tonimatas/discordmk/Main.java
+++ controller/src/main/java/dev/tonimatas/discordmk/Main.java
@@ -3,18 +3,49 @@ package dev.tonimatas.discordmk;
 import dev.tonimatas.discordmk.console.CommandsMKImpl;
 import dev.tonimatas.discordmk.console.ConsoleThread;
 import dev.tonimatas.discordmk.console.LoggerMK;
+import dev.tonimatas.discordmk.network.NetworkUtils;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.Properties;
 
 public class Main {
-    private static ConsoleThread consoleThread;
-    
     public static void main(String[] args) {
-        consoleThread = new ConsoleThread(new CommandsMKImpl());
-        consoleThread.start();
-        LoggerMK.info("Server started successfully.");
+        // TODO: Properties
+        Properties properties = new Properties();
+        
+        // TODO: Token
+        
+        // TODO: Connected server list
+
+        String port = properties.getProperty("port");
+        
+        try {
+            NetworkUtils.serverSocket = new ServerSocket(Integer.parseInt(port));
+        } catch (IOException e) {
+            LoggerMK.error("Error on create the server socket.");
+        }
+        
+        if (NetworkUtils.serverSocket == null) stop();
+        NetworkUtils.initServer(NetworkUtils.serverSocket);
+        LoggerMK.info("Server uses port: " + port);
+        
+        Common.consoleThread = new ConsoleThread(new CommandsMKImpl());
+        Common.consoleThread.start();
+        
+        LoggerMK.info("Controller started successfully.");
     }
 
     public static void stop() {
-        consoleThread.stopConsole();
-        LoggerMK.info("Server stopped correctly.");
+        Common.stopped = true;
+        Common.consoleThread.stopConsole();
+        
+        try {
+            NetworkUtils.serverSocket.close();
+        } catch (IOException e) {
+            LoggerMK.error("Error on close the server socket.");
+        }
+        
+        LoggerMK.info("Controller stopped correctly.");
     }
 }
