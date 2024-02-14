5b4aec041c9c99943408a0de1b44e4599bc1cc23
TonimatasDEV <amatascubero@gmail.com>
2024-02-14 20:29:50 +0100

    Basic networking

---
+++ common/src/main/java/dev/tonimatas/discordmk/Common.java
@@ -0,0 +1,8 @@
+package dev.tonimatas.discordmk;
+
+import dev.tonimatas.discordmk.console.ConsoleThread;
+
+public class Common {
+    public static boolean stopped = false;
+    public static ConsoleThread consoleThread;
+}
--- server/src/main/java/dev/tonimatas/discordmk/Main.java
+++ server/src/main/java/dev/tonimatas/discordmk/Main.java
@@ -6,9 +6,12 @@ import dev.tonimatas.discordmk.api.Bot;
 import dev.tonimatas.discordmk.console.CommandsMKImpl;
 import dev.tonimatas.discordmk.console.ConsoleThread;
 import dev.tonimatas.discordmk.console.LoggerMK;
+import dev.tonimatas.discordmk.network.NetworkUtils;
 import dev.tonimatas.discordmk.reader.ReaderMK;
 
 import java.io.File;
+import java.io.IOException;
+import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -16,7 +19,6 @@ import java.util.List;
 public class Main {
     public static Gson gson = new GsonBuilder().setPrettyPrinting().create();
     public static List<Bot> bots = new ArrayList<>();
-    private static ConsoleThread consoleThread;
     
     @SuppressWarnings("ResultOfMethodCallIgnored")
     public static void main(String[] args) {
@@ -35,14 +37,32 @@ public class Main {
         LoggerMK.info("Bots started successfully.");
         // TODO: Add the server logic to connect with the Controller.
 
-        consoleThread = new ConsoleThread(new CommandsMKImpl());
-        consoleThread.start();
+        Common.consoleThread = new ConsoleThread(new CommandsMKImpl());
+        Common.consoleThread.start();
+        
+        try {
+            // TODO: Possibility to change host and port
+            NetworkUtils.socket = new Socket("host", 2555);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        
+        NetworkUtils.initClient();
+
         LoggerMK.info("Server started successfully.");
     }
     
     public static void stop() {
-        consoleThread.stopConsole();
+        Common.stopped = true;
+        Common.consoleThread.stopConsole();
         bots.forEach(Bot::stop);
+
+        try {
+            NetworkUtils.socket.close();
+        } catch (IOException e) {
+            LoggerMK.error("Error on close the socket.");
+        }
+        
         LoggerMK.info("Server stopped correctly.");
     }
 }
