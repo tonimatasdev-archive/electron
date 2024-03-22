df0a74d3ef4c36749fe08b2acedb8c716af747df
TonimatasDEV <79017964+TonimatasDEV@users.noreply.github.com>
2024-03-22 17:34:53 +0100

    Networking now works fine!

--- common/src/main/java/dev/tonimatas/discordmk/Common.java
+++
@@ -1,8 +0,0 @@
-package dev.tonimatas.discordmk;
-
-import dev.tonimatas.discordmk.console.ConsoleThread;
-
-public class Common {
-    public static boolean stopped = false;
-    public static ConsoleThread consoleThread;
-}
---
+++ controller/src/main/java/dev/tonimatas/discordmk/console/CommandsMK.java
@@ -0,0 +1,26 @@
+package dev.tonimatas.discordmk.console;
+
+import dev.tonimatas.discordmk.Main;
+import dev.tonimatas.discordmk.network.Messages;
+
+import java.net.Socket;
+
+public class CommandsMK {
+    public static void runCommand(String command) {
+        switch (command) {
+            case "stop" -> handleStop();
+            case "test" -> handleTest();
+            default -> LoggerMK.error("Command \"" + command + "\" not found.");
+        }
+    }
+    
+    private static void handleStop() {
+        Main.stop();
+    }
+    
+    private static void handleTest() {
+        for (Socket socket : Main.clients) {
+            Messages.send(socket, "Hello");
+        }
+    }
+}
