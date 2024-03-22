495ce5b9654088cf77ab4360453ba10c7371cd3c
TonimatasDEV <79017964+TonimatasDEV@users.noreply.github.com>
2024-03-22 18:10:25 +0100

    Use LoggerMK.Type and delete unnecessary common

--- common/src/main/java/dev/tonimatas/discordmk/console/LoggerMK.java
+++ common/src/main/java/dev/tonimatas/discordmk/console/LoggerMK.java
@@ -5,19 +5,27 @@ import java.time.format.DateTimeFormatter;
 
 public class LoggerMK {
     public static void info(String message) {
-        send("\u001B[32m", "INFO", message);
+        send("\u001B[32m", Type.INFO, message);
     }
 
     public static void warn(String message) {
-        send("\u001B[33m", "WARN", message);
+        send("\u001B[33m", Type.WARN, message);
     }
 
     public static void error(String message) {
-        send("\u001B[31m", "ERROR", message);
+        send("\u001B[31m", Type.ERROR, message);
     }
 
-    private static void send(String color, String type, String message) {
-        System.out.println(color + "[" + getDate() + "] [" + type + "]: " + message);
+    public static void send(Type type, String message) {
+        switch (type) {
+            case INFO -> info(message);
+            case WARN -> warn(message);
+            case ERROR -> error(message);
+        }
+    }
+    
+    private static void send(String color, Type type, String message) {
+        System.out.println(color + "[" + getDate() + "] [" + type.name().toUpperCase() + "]: " + message);
         System.out.print("\u001B[0m");
     }
 
@@ -25,7 +33,7 @@ public class LoggerMK {
         return LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss"));
     }
     
-    public enum Types {
+    public enum Type {
         ERROR,
         WARN,
         INFO
--- controller/src/main/java/dev/tonimatas/discordmk/util/Network.java
+++ controller/src/main/java/dev/tonimatas/discordmk/util/Network.java
@@ -1,19 +1,18 @@
-package dev.tonimatas.discordmk.network;
+package dev.tonimatas.discordmk.util;
 
 import dev.tonimatas.discordmk.console.LoggerMK;
 
-import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 
-public class Messages {
+public class Network {
     public static boolean send(Socket socket, String message) {
         if (socket.isClosed()) {
             LoggerMK.error("The server " + socket.getInetAddress() + " is closed.");
             return false;
         }
-        
+
         try {
             DataOutputStream out = new DataOutputStream(socket.getOutputStream());
             out.writeUTF(message);
@@ -22,15 +21,6 @@ public class Messages {
         } catch (IOException e) {
             LoggerMK.error("Error on send the message to " + socket.getInetAddress());
             return false;
-        }
-    }
-
-    public static String receive(Socket socket) {
-        try {
-            DataInputStream in = new DataInputStream(socket.getInputStream());
-            return in.readUTF();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
         }
     }
 }
