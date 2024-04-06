7408408611657c34bec805b50ddd4e6a3a60df6a
TonimatasDEV <79017964+TonimatasDEV@users.noreply.github.com>
2024-04-06 15:38:12 +0200

    New things to create

---
+++ api/src/main/java/dev/tonimatas/discordmk/api/function/voice/MuteMK.java
@@ -0,0 +1,5 @@
+package dev.tonimatas.discordmk.api.function.voice;
+
+public class MuteMK {
+    // TODO: Create this
+}
---
+++ api/src/main/java/dev/tonimatas/discordmk/api/function/voice/TimeoutMK.java
@@ -0,0 +1,5 @@
+package dev.tonimatas.discordmk.api.function.voice;
+
+public class TimeoutMK {
+    // TODO: Create this
+}
