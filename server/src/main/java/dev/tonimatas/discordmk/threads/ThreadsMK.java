7c818f32b731d977aeffdaf4e1b726fc2727c863
TonimatasDEV <79017964+TonimatasDEV@users.noreply.github.com>
2024-04-10 20:03:48 +0200

    Fix problems when the controller connection is interrupted

--- server/src/main/java/dev/tonimatas/discordmk/threads/ThreadsMK.java
+++ server/src/main/java/dev/tonimatas/discordmk/threads/ThreadsMK.java
@@ -2,10 +2,12 @@ package dev.tonimatas.discordmk.threads;
 
 import dev.tonimatas.discordmk.Main;
 import dev.tonimatas.discordmk.console.CommandsMK;
+import dev.tonimatas.discordmk.console.LoggerMK;
 import dev.tonimatas.discordmk.console.TasksMK;
 
 import java.io.DataInputStream;
 import java.io.IOException;
+import java.net.Socket;
 import java.util.Scanner;
 
 public class ThreadsMK {
@@ -21,14 +23,51 @@ public class ThreadsMK {
 
     public static void initReceiveThread() {
         new Thread(() -> {
-            while (!Main.stopped) {
+            while (!Main.stopped && Main.socket != null) {
                 try {
                     DataInputStream in = new DataInputStream(Main.socket.getInputStream());
                     TasksMK.runTask(in.readUTF());
-                } catch (IOException e) {
+                } catch (IOException ignored) {
+                    Main.socket = null;
+                }
+            }
+        }).start();
+    }
+    
+    public static void initCheckConnection(String host, int port) {
+        new Thread(() -> {
+            while (!Main.stopped) {
+                if (Main.socket == null) {
+                    initConnectionThread(host, port);
+                }
+                
+                try {
+                    //noinspection BusyWait
+                    Thread.sleep(250);
+                } catch (InterruptedException e) {
                     throw new RuntimeException(e);
                 }
             }
         }).start();
+    }
+
+    public static void initConnectionThread(String host, int port) {
+        while (!Main.stopped && Main.socket == null) {
+            try {
+                Main.socket = new Socket(host, port);
+            } catch (IOException e) {
+                LoggerMK.error("Error on connect to the server " + host + ":" + port + ". Next try in 30 seconds.");
+                
+                try {
+                    //noinspection BusyWait
+                    Thread.sleep(30 * 1000);
+                } catch (InterruptedException exception) {
+                    throw new RuntimeException(exception);
+                }
+            }
+        }
+        
+        LoggerMK.info("Successfully connected to the controller " + host + ":" + port);
+        ThreadsMK.initReceiveThread();
     }
 }
