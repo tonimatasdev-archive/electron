c74c2cfd2dd6773a7e7853bc0585017291eeab21
TonimatasDEV <amatascubero@gmail.com>
2024-05-16 20:08:30 +0200

    Add the RestController

--- controller/src/main/java/dev/tonimatas/electron/api/ServletInitializerMK.java
+++ controller/src/main/java/dev/tonimatas/electron/api/ServletInitializerMK.java
@@ -4,7 +4,7 @@ import dev.tonimatas.electron.ControllerMK;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-public class ServletInitializer extends SpringBootServletInitializer {
+public class ServletInitializerMK extends SpringBootServletInitializer {
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(ControllerMK.class);
