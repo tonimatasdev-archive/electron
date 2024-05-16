7e365ed77e45ba7a4e2887efe52b5983c1b3b069
TonimatasDEV <79017964+TonimatasDEV@users.noreply.github.com>
2024-05-16 20:08:30 +0200

    Add the RestController

---
+++ controller/src/main/java/dev/tonimatas/electron/api/RestControllerMK.java
@@ -0,0 +1,8 @@
+package dev.tonimatas.electron.api;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RestControllerMK {
+    // TODO: A lot of things to do here
+}
