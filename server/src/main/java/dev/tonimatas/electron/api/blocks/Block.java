f8dbc23907ff4493500234be6157a3dd7800c043
TonimatasDEV <79017964+TonimatasDEV@users.noreply.github.com>
2024-06-16 20:30:34 +0200

    [skip ci] Change the name of artifact

--- server/src/main/java/dev/tonimatas/electron/api/blocks/Block.java
+++
@@ -1,9 +0,0 @@
-package dev.tonimatas.electron.api.blocks;
-
-public abstract class Block implements IBlock {
-    protected String id;
-    
-    public Block(String id) {
-        this.id = id;
-    }
-}
