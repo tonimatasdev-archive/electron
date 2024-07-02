953aa5cdced615a1914a5a9438be2a22421d76d3
TonimatasDEV <79017964+TonimatasDEV@users.noreply.github.com>
2024-07-03 00:52:59 +0200

    Only server

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     java
+    id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
 val projectVersion: String by extra
@@ -7,33 +8,43 @@ val jdaVersion: String by extra
 
 base.archivesName.set("electron")
 
-allprojects {
-    apply(plugin = "java")
-
-    java {
-        sourceCompatibility = JavaVersion.VERSION_21
-        targetCompatibility = JavaVersion.VERSION_21
-    }
-
-    version = projectVersion
-    group = "dev.tonimatas"
-
-    repositories {
-        mavenCentral()
-    }
-
-    tasks.withType<JavaCompile> {
-        options.encoding = "UTF-8"
-    }
-
-    java.withSourcesJar()
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
 }
 
+version = projectVersion
+group = "dev.tonimatas"
 
-subprojects {
-    if (project.name == "controller") return@subprojects
+repositories {
+    mavenCentral()
+    mavenLocal()
+}
 
-    dependencies {
-        implementation("net.dv8tion:JDA:$jdaVersion")
-    }
+dependencies {
+    implementation("dev.discordmk:quark:1.0.0")
+    implementation("net.dv8tion:JDA:$jdaVersion")
+    implementation("ch.qos.logback:logback-classic:1.5.6")
+    
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+}
+
+java.withSourcesJar()
+
+tasks.shadowJar {
+    archiveClassifier.set("")
+    mergeServiceFiles()
+
+    manifest.attributes("Main-Class" to "dev.tonimatas.electron.ServerMK")
+}
+
+tasks.jar {
+    archiveClassifier.set("plain")
+}
+
+tasks.create("exportJar") {
+    dependsOn(tasks.shadowJar)
 }
